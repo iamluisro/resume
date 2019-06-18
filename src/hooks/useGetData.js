d0e418a9047c9a5271e8fb75ea60fcae447cd2c2
lfrodriguez <lfrodriguez06@gmail.com>
2019-06-18 17:39:33 -0500

    installed firebase

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,13 +1,14 @@
-import { useState, useEffect } from "react";
-const api = "https://us-central1-gndx-cv.cloudfunctions.net/me";
+import { useState, useEffect } from 'react';
+const api = 'https://us-central1-gndx-cv.cloudfunctions.net/me';
 
 const useGetData = () => {
-	const [mydata, setData] = useState([]);
+	const [
+		mydata,
+		setData
+	] = useState([]);
 
 	useEffect(() => {
-		fetch(api)
-			.then(response => response.json())
-			.then(data => setData(data));
+		fetch(api).then((response) => response.json()).then((data) => setData(data));
 	}, []);
 
 	return mydata;
