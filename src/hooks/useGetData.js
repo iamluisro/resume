29d2c18cb98b01ca75c638a7e485f6916b49f160
lfrodriguez <lfrodriguez06@gmail.com>
2019-06-19 14:15:30 -0500

    Created firebase API and hooked it into the project

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-const api = 'https://us-central1-gndx-cv.cloudfunctions.net/me';
+const api = 'https://us-central1-luis-rodriguez-resume.cloudfunctions.net/api';
 
 const useGetData = () => {
 	const [
